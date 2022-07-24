bcb4d24c5bd2a6785a0527d729edf6c80d598f06
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-25 06:41:17 +1000

    Update scanner for dirs

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -1,4 +1,6 @@
-use chrono::Utc;
+use std::time::SystemTime;
+
+use chrono::{DateTime, Utc};
 use entity;
 use migration::OnConflict;
 use sea_orm::{ActiveValue::NotSet, DatabaseConnection, EntityTrait, Set};
@@ -21,18 +23,47 @@ pub async fn walk(db: &DatabaseConnection) -> anyhow::Result<()> {
     tracing::info!("Starting scan");
 
     let current_dir = "H:\\Music";
+
+    let dirs = entity::directories::Entity::find().all(db).await?;
+    println!("{:?}", dirs.len());
     for entry in WalkDir::new(current_dir)
         .follow_links(true)
         .into_iter()
         .filter_map(|e| e.ok())
     {
+        let path: String = entry.path().to_string_lossy().to_string();
+
+        if entry.file_type().is_dir() {
+            let fmtime: SystemTime = entry.metadata().unwrap().modified().unwrap();
+            let mtime: DateTime<Utc> = fmtime.into();
+            let init_time: String = Utc::now().naive_local().to_string();
+            let dir = entity::directories::ActiveModel {
+                id: Set(Uuid::new_v4().to_string()),
+                path: Set(path.clone()),
+                mtime: Set(mtime.naive_utc()),
+                created_at: Set(init_time.clone()),
+                updated_at: Set(init_time),
+            };
+
+            entity::directories::Entity::insert(dir)
+                .on_conflict(
+                    // on conflict do nothing
+                    OnConflict::column(entity::songs::Column::Path)
+                        .do_nothing()
+                        .to_owned(),
+                )
+                .exec(db)
+                .await
+                .expect("Failed to insert song");
+        }
         let f_name = entry.file_name().to_string_lossy();
         if f_name.ends_with(".flac") {
-            let metadata = skip_fail!(tag_helper::get_metadata(
-                entry.path().to_string_lossy().to_string()
-            ));
+            let metadata = skip_fail!(tag_helper::get_metadata(path));
 
             let id = Uuid::new_v4();
+
+            let init_time: String = Utc::now().naive_local().to_string();
+
             let song = entity::songs::ActiveModel {
                 id: Set(id.to_string()),
                 path: Set(metadata.path),
@@ -49,8 +80,8 @@ pub async fn walk(db: &DatabaseConnection) -> anyhow::Result<()> {
                 music_brainz_recording_id: NotSet,
                 music_brainz_artist_id: NotSet,
                 music_brainz_track_id: NotSet,
-                created_at: Set(Utc::now().naive_local().to_string()),
-                updated_at: Set(Utc::now().naive_local().to_string()),
+                created_at: Set(init_time.clone()),
+                updated_at: Set(init_time),
                 album_id: NotSet,
             };
 
@@ -58,7 +89,7 @@ pub async fn walk(db: &DatabaseConnection) -> anyhow::Result<()> {
                 .on_conflict(
                     // on conflict do nothing
                     OnConflict::column(entity::songs::Column::Path)
-                        .do_nothing()
+                        .update_column(entity::songs::Column::UpdatedAt)
                         .to_owned(),
                 )
                 .exec(db)
