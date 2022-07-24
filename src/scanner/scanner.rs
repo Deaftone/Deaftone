ffccadbd463f89e3b93fc5cddb4e55086cfeef0e
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-25 02:18:06 +1000

    Init scanner

---
+++ src/scanner/scanner.rs
@@ -0,0 +1,93 @@
+use chrono::Utc;
+use entity;
+use metaflac::Tag;
+use sea_orm::{
+    ActiveModelTrait, ActiveValue::NotSet, ColumnTrait, DatabaseConnection, EntityTrait,
+    QueryFilter, Set,
+};
+use uuid::Uuid;
+use walkdir::WalkDir;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct AudioMetadata {
+    pub name: String,
+    pub number: u32,
+    pub album: String,
+    pub album_artist: String,
+    pub year: i32,
+    pub track: u32,
+    pub artists: Vec<String>,
+    pub path: std::path::PathBuf,
+    pub lossless: bool,
+}
+
+pub async fn walk(db: &DatabaseConnection) -> anyhow::Result<()> {
+    let current_dir = "G:\\aa";
+    for entry in WalkDir::new(current_dir)
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let f_name = entry.file_name().to_string_lossy();
+
+        if f_name.ends_with(".flac") {
+            println!("{}", f_name);
+            let tag = Tag::read_from_path(entry.path()).unwrap();
+            let vorbis = tag.vorbis_comments().ok_or(0).unwrap();
+            let year = vorbis
+                .comments
+                .get("YEAR")
+                .and_then(|d| d[0].parse::<i32>().ok());
+
+            let metadata = AudioMetadata {
+                name: vorbis.title().map(|v| v[0].clone()).unwrap(),
+                number: vorbis.track().unwrap(),
+                album: vorbis.album().map(|v| v[0].clone()).unwrap(),
+                album_artist: match vorbis.album_artist().map(|v| v[0].clone()) {
+                    Some(e) => e,
+                    None => vorbis.artist().map(|v| v[0].clone()).unwrap(),
+                },
+                year: year.unwrap_or(0),
+                track: vorbis.track().unwrap(),
+                artists: vorbis.artist().unwrap().to_owned(),
+                path: entry.path().to_owned(),
+                lossless: true,
+            };
+            let path = entry.path().to_string_lossy().to_string();
+            let id = Uuid::new_v4();
+            let song = entity::songs::ActiveModel {
+                id: Set(id.to_string()),
+                path: Set(path),
+                title: Set(metadata.name),
+                disk: NotSet,
+                artist: Set(metadata.album_artist),
+                album_name: Set(metadata.album),
+                codec: NotSet,
+                sample_rate: NotSet,
+                bits_per_sample: NotSet,
+                track: NotSet,
+                year: Set(year),
+                label: NotSet,
+                music_brainz_recording_id: NotSet,
+                music_brainz_artist_id: NotSet,
+                music_brainz_track_id: NotSet,
+                created_at: Set(Utc::now().naive_local()),
+                updated_at: Set(Utc::now().naive_local()),
+                album_id: NotSet,
+            };
+            /*      .insert(db)
+            .await
+            .expect("Failed to insert"); */
+
+            let _song = entity::songs::Entity::find()
+                .filter(entity::songs::Column::Path.contains(&entry.path().to_string_lossy().to_string()))
+                .all(db)
+                .await
+                .expect("Failed to find song")
+
+                
+        }
+    }
+
+    Ok(())
+}
