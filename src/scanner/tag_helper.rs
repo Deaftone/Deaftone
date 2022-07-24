c5a87e6037c136285a3099d4b2efea52e27a7fb2
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-25 06:01:52 +1000

    Default value errors

--- src/scanner/tag_helper.rs
+++ src/scanner/tag_helper.rs
@@ -8,7 +8,6 @@ pub struct AudioMetadata {
     pub album_artist: String,
     pub year: i32,
     pub track: u32,
-    pub artists: Vec<String>,
     pub path: String,
     pub lossless: bool,
 }
@@ -23,36 +22,61 @@ pub fn get_metadata(path: String) -> Result<AudioMetadata> {
         name: vorbis
             .title()
             .map(|v| v[0].clone())
-            .with_context(|| "Failed to read title")?,
+            .unwrap_or("FAILED TO READ TITLE DEAFTONE".to_string()),
         number: vorbis.track().unwrap_or_default(),
         album: vorbis
             .album()
             .map(|v| v[0].clone())
-            .with_context(|| "Failed to read album")?,
+            .unwrap_or("FAILED TO READ ALBUM DEAFTONE".to_string()),
         album_artist: match vorbis.album_artist().map(|v| v[0].clone()) {
             Some(e) => e,
             None => vorbis
                 .artist()
                 .map(|v| v[0].clone())
-                .with_context(|| "Failed to read album_artist")?,
+                .unwrap_or("FAILED TO READ ARTIST DEAFTONE".to_string()),
         },
         year: get_year(vorbis).with_context(|| "Failed to read year")?,
-        track: vorbis.track().with_context(|| "Failed to read track")?,
-        artists: vorbis
-            .artist()
-            .with_context(|| "Failed to read artist")?
-            .to_owned(),
+        track: vorbis.track().unwrap_or(0),
         path,
         lossless: true,
     };
     Ok(metadata)
 }
-
+// This is ugly. But why is there 3 different tags for date?
 fn get_year(vorbis: &VorbisComment) -> Result<i32> {
-    let year = vorbis
+    let original_year = vorbis
         .comments
         .get("ORIGINALYEAR")
-        .and_then(|d| d[0].parse::<i32>().ok())
-        .unwrap();
-    Ok(year)
+        .and_then(|d| d[0].parse::<String>().ok())
+        .unwrap_or_default();
+
+    let date = vorbis
+        .comments
+        .get("DATE")
+        .and_then(|d| d[0].parse::<String>().ok())
+        .unwrap_or_default();
+    let year = vorbis
+        .comments
+        .get("YEAR")
+        .and_then(|d| d[0].parse::<String>().ok())
+        .unwrap_or_default();
+
+    if year.chars().count() >= 4 {
+        Ok(parse_year(year).with_context(|| "Failed to parse YEAR")?)
+    } else if date.chars().count() >= 4 {
+        Ok(parse_year(date).with_context(|| "Failed to parse DATE")?)
+    } else if original_year.chars().count() >= 4 {
+        Ok(parse_year(original_year).with_context(|| "Failed to parse ORIGINALDATE")?)
+    } else {
+        Ok(0)
+    }
+}
+
+fn parse_year(mut year: String) -> Result<i32> {
+    if year.chars().count() == 10 {
+        year.truncate(4);
+        Ok(year.parse::<i32>().unwrap_or_default())
+    } else {
+        Ok(year.parse::<i32>().unwrap_or_default())
+    }
 }
