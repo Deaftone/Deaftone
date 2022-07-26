16b4c8cc9cbb9f8d89947131095d8c99e394f87c
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-27 02:08:40 +1000

    Add test stream handler

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use axum::{response::Html, routing::get, Extension, Router};
-
+use anyhow::{Ok, Result};
+use axum::{
     body::Body,
     extract::Path,
-use std::{env, fs};
-use std::{net::SocketAddr, time::Duration};
-use tower_http::trace::TraceLayer;
+    http::Request,
+    response::Html,
+    response::{IntoResponse, Response},
+    routing::get,
+    Extension, Router,
+};
+use db::song_repo;
+use scanner::Scanner;
+use sea_orm::DatabaseConnection;
+use std::env;
+use std::net::SocketAddr;
+use tower::Service;
+use tower_http::{services::ServeFile, trace::TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod db;
 mod scanner;
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<()> {
     env::set_var("RUST_LOG", "info");
 
     // Setup tracing logger
@@ -23,16 +33,20 @@ async fn main() -> anyhow::Result<()> {
         .init();
 
     // Connecting SQLite
-    let before = Instant::now();
 
     let db = db::get_connection().await?;
     db::migrate_up(&db).await?;
 
+    let mut scan = scanner::Scanner::new().unwrap();
+    scan.start_scan();
+    println!("{:?}", scan.get_status());
     // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
+        .route("/stream/:id", get(stream_handler))
         .layer(TraceLayer::new_for_http())
-        .layer(Extension(db));
+        .layer(Extension(db))
+        .layer(Extension(scan));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -44,7 +58,28 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn handler(Extension(ref _pool): Extension<DatabaseConnection>) -> Html<&'static str> {
+struct Params {
+    song_id: String,
+}
+
+async fn stream_handler(
+    Path(song_id): Path<String>,
+    Extension(ref db): Extension<DatabaseConnection>,
+    req: Request<Body>,
+) -> impl IntoResponse {
+    let song = song_repo::get_song(db, song_id)
+        .await
+        .expect("Unknown Song");
+    let path = song.unwrap().path;
+    println!("{:?}", path);
+
+    return ServeFile::new(path).call(req).await.unwrap();
+}
+
+async fn handler(
+    Extension(ref scanner): Extension<Scanner>,
+    Extension(ref db): Extension<DatabaseConnection>,
+) -> Html<&'static str> {
     /*     let id = Uuid::new_v4();
     entity::artists::ActiveModel {
         id: Set(id.to_string().to_owned()),
@@ -57,5 +92,6 @@ async fn handler(Extension(ref _pool): Extension<DatabaseConnection>) -> Html<&'
     .insert(pool)
     .await
     .expect("Failed to insert"); */
-    Html("<h1>Hello, World!</h1>")
+    println!("{:?}", scanner.get_status());
+    Html("<h1>{Hello, World}!</h1>")
 }
