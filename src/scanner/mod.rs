fa326b86e905144b5272a0f85f78e41966ef4aa6
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-27 02:08:31 +1000

    Move scanner to impl

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,2 +1,34 @@
+use crate::db::get_connection;
+use anyhow::Result;
+use std::sync::{atomic::AtomicBool, Arc};
+
 pub mod scanner;
 pub mod tag_helper;
+#[derive(Clone)]
+pub struct Scanner {
+    scanning: bool,
+}
+impl Scanner {
+    pub fn new() -> Result<Scanner> {
+        let scanner = Scanner { scanning: false };
+        Ok(scanner)
+    }
+
+    fn update_scanning(&mut self, status: bool) {
+        self.scanning = status
+    }
+    pub fn get_status(&self) -> bool {
+        return self.scanning;
+    }
+    pub fn start_scan(&mut self) {
+        tokio::spawn(async move {
+            let db_connection = get_connection().await.expect("Failed to connect to db");
+            use std::time::Instant;
+            let before = Instant::now();
+            scanner::walk(&db_connection).await.unwrap();
+            tracing::info!("Scan completed in: {:.2?}", before.elapsed());
+            //self.update_scanning(AtomicBool::new(false))
+            //val = Arc::new(false);
+        });
+    }
+}
