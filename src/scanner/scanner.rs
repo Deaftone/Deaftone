487f2d7408821e8bac5fafdee5ec86dada7800b5
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-28 07:22:20 +1000

    Add artist and album inserts

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -1,9 +1,12 @@
 use anyhow::Result;
 use chrono::{DateTime, Utc};
 use entity;
-use migration::OnConflict;
-use sea_orm::{ActiveValue::NotSet, DatabaseConnection, EntityTrait, Set};
-use std::time::SystemTime;
+use migration::{Expr, OnConflict};
+use sea_orm::{
+    ActiveValue::NotSet, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, QuerySelect,
+    Set,
+};
+use std::{ops::Not, time::SystemTime};
 use uuid::Uuid;
 use walkdir::{DirEntry, WalkDir};
 
@@ -20,55 +23,6 @@ macro_rules! skip_fail {
     };
 }
 
-pub async fn process_directory(
-    db: &DatabaseConnection,
-    entry: DirEntry,
-    path: String,
-    dirs: &Vec<entity::directories::Model>,
-) -> Result<bool> {
-    let index = dirs.iter().position(|r| r.path == path);
-    let fmtime: SystemTime = entry.metadata().unwrap().modified().unwrap();
-    let mtime: DateTime<Utc> = fmtime.into();
-    if index == None {
-        tracing::info!("New dir found scanning... {:?}", path);
-        insert_directory(&path, &mtime, db).await?;
-        return Ok(true);
-    } else if dirs[index.unwrap()].mtime > mtime.naive_utc() {
-        tracing::info!("Dir changed {}", path);
-        insert_directory(&path, &mtime, db).await?;
-        return Ok(true);
-    } else {
-        tracing::debug!("Dir hasn't {}", path);
-        Ok(false)
-    }
-}
-
-pub async fn insert_directory(
-    path: &String,
-    mtime: &DateTime<Utc>,
-    db: &DatabaseConnection,
-) -> Result<()> {
-    let init_time: String = Utc::now().naive_local().to_string();
-    let dir = entity::directories::ActiveModel {
-        id: Set(Uuid::new_v4().to_string()),
-        path: Set(path.to_owned()),
-        mtime: Set(mtime.naive_utc()),
-        created_at: Set(init_time.to_owned()),
-        updated_at: Set(init_time),
-    };
-
-    entity::directories::Entity::insert(dir)
-        .on_conflict(
-            OnConflict::column(entity::directories::Column::Path)
-                .update_column(entity::directories::Column::UpdatedAt)
-                .update_column(entity::directories::Column::Mtime)
-                .to_owned(),
-        )
-        .exec(db)
-        .await
-        .expect("Failed to insert dir");
-    Ok(())
-}
 pub async fn walk(db: &DatabaseConnection) -> Result<()> {
     tracing::info!("Starting scan");
     let dirs = entity::directories::Entity::find().all(db).await?;
@@ -126,5 +80,146 @@ pub async fn walk(db: &DatabaseConnection) -> Result<()> {
         }
     }
 
+    Ok(())
+}
+
+pub async fn create_artists(db: &DatabaseConnection) {
+    let songs = entity::songs::Entity::find()
+        .group_by(entity::songs::Column::Artist)
+        .all(db)
+        .await
+        .unwrap();
+
+    for song in songs.iter() {
+        let id = Uuid::new_v4();
+        let init_time: String = Utc::now().naive_local().to_string();
+
+        let artist = entity::artists::ActiveModel {
+            id: Set(id.to_string()),
+            name: Set(song.artist.to_owned()),
+            image: NotSet,
+            bio: NotSet,
+            created_at: Set(init_time.to_owned()),
+            updated_at: Set(init_time),
+        };
+        entity::artists::Entity::insert(artist)
+            .on_conflict(
+                OnConflict::column(entity::artists::Column::Name)
+                    .do_nothing()
+                    .to_owned(),
+            )
+            .exec(db)
+            .await
+            .expect("Failed to insert song");
+    }
+    update_artist_id(&db).await;
+}
+
+async fn update_artist_id(db: &DatabaseConnection) {
+    let artists = entity::artists::Entity::find()
+        .group_by(entity::artists::Column::Name)
+        .all(db)
+        .await
+        .unwrap();
+
+    for artist in artists.iter() {
+        entity::albums::Entity::update_many()
+            .col_expr(
+                entity::albums::Column::ArtistId,
+                Expr::value(artist.id.to_owned()),
+            )
+            .filter(entity::albums::Column::ArtistName.eq(artist.name.to_owned()))
+            .exec(db)
+            .await
+            .expect("Failed to insert album");
+    }
+}
+pub async fn create_albums(db: &DatabaseConnection) {
+    let songs = entity::songs::Entity::find()
+        .group_by(entity::songs::Column::Artist)
+        .group_by(entity::songs::Column::AlbumName)
+        .all(db)
+        .await
+        .unwrap();
+
+    for song in songs.iter() {
+        let id = Uuid::new_v4();
+        let init_time: String = Utc::now().naive_local().to_string();
+
+        let albums = entity::albums::ActiveModel {
+            id: Set(id.to_string()),
+            name: Set(song.album_name.to_owned()),
+            artist_name: Set(song.artist.to_owned()),
+            year: Set(song.year.unwrap_or_default()),
+            created_at: Set(init_time.to_owned()),
+            updated_at: Set(init_time),
+            artist_id: NotSet,
+        };
+        entity::albums::Entity::insert(albums)
+            /*           .on_conflict(
+                // on conflict do nothing
+                OnConflict::column(entity::albums::Column::Name)
+                    .do_nothing()
+                    .to_owned(),
+            ) */
+            .exec(db)
+            .await
+            .expect("Failed to insert album");
+
+        entity::songs::Entity::update_many()
+            .col_expr(entity::songs::Column::AlbumId, Expr::value(id.to_string()))
+            .filter(entity::songs::Column::AlbumName.eq(song.album_name.to_string()))
+            .exec(db)
+            .await
+            .unwrap();
+    }
+}
+pub async fn process_directory(
+    db: &DatabaseConnection,
+    entry: DirEntry,
+    path: String,
+    dirs: &Vec<entity::directories::Model>,
+) -> Result<bool> {
+    let index = dirs.iter().position(|r| r.path == path);
+    let fmtime: SystemTime = entry.metadata().unwrap().modified().unwrap();
+    let mtime: DateTime<Utc> = fmtime.into();
+    if index == None {
+        tracing::info!("New dir found scanning... {:?}", path);
+        insert_directory(&path, &mtime, db).await?;
+        return Ok(true);
+    } else if dirs[index.unwrap()].mtime > mtime.naive_utc() {
+        tracing::info!("Dir changed {}", path);
+        insert_directory(&path, &mtime, db).await?;
+        return Ok(true);
+    } else {
+        tracing::debug!("Dir hasn't {}", path);
+        Ok(false)
+    }
+}
+
+pub async fn insert_directory(
+    path: &String,
+    mtime: &DateTime<Utc>,
+    db: &DatabaseConnection,
+) -> Result<()> {
+    let init_time: String = Utc::now().naive_local().to_string();
+    let dir = entity::directories::ActiveModel {
+        id: Set(Uuid::new_v4().to_string()),
+        path: Set(path.to_owned()),
+        mtime: Set(mtime.naive_utc()),
+        created_at: Set(init_time.to_owned()),
+        updated_at: Set(init_time),
+    };
+
+    entity::directories::Entity::insert(dir)
+        .on_conflict(
+            OnConflict::column(entity::directories::Column::Path)
+                .update_column(entity::directories::Column::UpdatedAt)
+                .update_column(entity::directories::Column::Mtime)
+                .to_owned(),
+        )
+        .exec(db)
+        .await
+        .expect("Failed to insert dir");
     Ok(())
 }
