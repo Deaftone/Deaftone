2bfb05971ed7bd50a1a67e485e4b443a32d27235
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-28 07:22:35 +1000

    Update connection

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -5,23 +5,34 @@ use std::{fs, time::Duration};
 
 pub mod song_repo;
 
-pub async fn get_connection() -> Result<DatabaseConnection> {
-    if fs::metadata("./deaftone.sqlite").is_err() {
-        fs::File::create("./deaftone.sqlite").expect("Created file");
-    }
-    let mut opt = ConnectOptions::new("sqlite://./deaftone.sqlite?mode=rwc".to_owned());
-    opt.max_connections(100)
-        .min_connections(5)
-        .connect_timeout(Duration::from_secs(8))
-        .idle_timeout(Duration::from_secs(8))
-        .max_lifetime(Duration::from_secs(8))
-        .sqlx_logging(false);
-
-    let db = Database::connect(opt).await?;
-    Ok(db)
+#[derive(Clone)]
+pub struct DB {
+    pool: DatabaseConnection,
 }
+impl DB {
+    pub async fn new() -> Result<DB> {
+        if fs::metadata("./deaftone.sqlite").is_err() {
+            fs::File::create("./deaftone.sqlite").expect("Created file");
+        }
+        let mut opt = ConnectOptions::new("sqlite://./deaftone.sqlite?mode=rwc".to_owned());
+        opt.max_connections(100)
+            .min_connections(5)
+            .connect_timeout(Duration::from_secs(8))
+            .idle_timeout(Duration::from_secs(8))
+            .max_lifetime(Duration::from_secs(8))
+            .sqlx_logging(false);
 
-pub async fn migrate_up(db: &DatabaseConnection) -> Result<()> {
-    Migrator::up(db, None).await?;
-    Ok(())
+        let pool = Database::connect(opt).await?;
+        let db = DB { pool };
+        db.migrate_up().await?;
+        Ok(db)
+    }
+
+    pub fn connect(self) -> DatabaseConnection {
+        return self.pool;
+    }
+    async fn migrate_up(&self) -> Result<()> {
+        Migrator::up(&self.pool, None).await?;
+        Ok(())
+    }
 }
