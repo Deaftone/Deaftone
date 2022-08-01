5b4876c3ab8e60cb6325fef815e6b6760bd949df
Rylee George <13013625+112RG@users.noreply.github.com>
2022-08-02 09:09:25 +1000

    Add indexes

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -27,11 +27,102 @@ pub async fn create_tables(db: &DbConn) {
     create_table(db, &schema, entity::artists::Entity).await;
     create_table(db, &schema, entity::directories::Entity).await;
 }
-
 #[async_trait::async_trait]
+#[allow(unused_must_use)]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         create_tables(manager.get_connection()).await;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&format!(
+                        "idx-{}-{}",
+                        entity::albums::Entity.to_string(),
+                        entity::albums::Column::Id.to_string()
+                    ))
+                    .table(entity::albums::Entity)
+                    .col(entity::albums::Column::Id)
+                    .to_owned(),
+            )
+            .await;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&format!(
+                        "idx-{}-{}",
+                        entity::songs::Entity.to_string(),
+                        entity::songs::Column::Id.to_string()
+                    ))
+                    .table(entity::songs::Entity)
+                    .col(entity::songs::Column::Id)
+                    .to_owned(),
+            )
+            .await;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&format!(
+                        "idx-{}-{}",
+                        entity::artists::Entity.to_string(),
+                        entity::artists::Column::Id.to_string()
+                    ))
+                    .table(entity::artists::Entity)
+                    .col(entity::artists::Column::Id)
+                    .to_owned(),
+            )
+            .await;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&format!(
+                        "idx-{}-{}",
+                        entity::directories::Entity.to_string(),
+                        entity::directories::Column::Id.to_string()
+                    ))
+                    .table(entity::directories::Entity)
+                    .col(entity::directories::Column::Id)
+                    .to_owned(),
+            )
+            .await;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&format!(
+                        "idx-{}-{}",
+                        entity::artists::Entity.to_string(),
+                        entity::artists::Column::Name.to_string()
+                    ))
+                    .table(entity::artists::Entity)
+                    .col(entity::artists::Column::Name)
+                    .to_owned(),
+            )
+            .await;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&format!(
+                        "idx-{}-{}",
+                        entity::albums::Entity.to_string(),
+                        entity::albums::Column::Name.to_string()
+                    ))
+                    .table(entity::albums::Entity)
+                    .col(entity::albums::Column::Name)
+                    .to_owned(),
+            )
+            .await;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&format!(
+                        "idx-{}-{}",
+                        entity::songs::Entity.to_string(),
+                        entity::songs::Column::Path.to_string()
+                    ))
+                    .table(entity::songs::Entity)
+                    .col(entity::songs::Column::Path)
+                    .to_owned(),
+            )
+            .await;
         Ok(())
     }
 
