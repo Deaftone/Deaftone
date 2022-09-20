a95106a49ed1b06c3b86ad553404b928824edcd3
Rylee George <ryleegeorge1@gmail.com>
2022-09-21 04:26:59 +1000

    Update settings

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -1,21 +1,26 @@
-use config::{Config, ConfigError, File, FileFormat};
+use std::collections::HashMap;
+
+use config::{Config, ConfigError};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Settings {
     pub log_level: String,
     pub db_path: String,
+    pub media_path: String,
 }
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
         let s = Config::builder()
-            .add_source(File::from_str(
-                include_str!("settings.default.toml"),
-                FileFormat::Toml,
-            ))
+            .add_source(config::File::with_name("settings.toml"))
             .build()?;
-
+        println!(
+            "{:?}",
+            s.to_owned()
+                .try_deserialize::<HashMap<String, String>>()
+                .unwrap()
+        );
         s.try_deserialize()
     }
 }
